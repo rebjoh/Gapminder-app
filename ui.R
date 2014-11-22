7a63d672f79a081fd46172de5b5737949041fe33
Rebecca Johnston <rebecca.lea.johnston@gmail.com>
2014-11-22 13:19:23 -0800

    Add dropdown menu to choose 2nd country

--- ui.R
+++ ui.R
@@ -4,21 +4,19 @@ shinyUI(fluidPage(
 	sidebarLayout(
 		sidebarPanel("Select 'Country' and 'Range of years' from Gapminder:",
 								 uiOutput("choose_country"),
-# 								 selectInput("select_country",
-# 								 						label = "Country",
-# 								 						choices = list("Chad", "Iraq", "Mali")),
+								 uiOutput("choose_country_2"),
 								 sliderInput("year_range",
 								 						label = "Range of years:",
 								 						min = 1952,
 								 						max = 2007,
-								 						value = c(1955, 2005), 
+								 						value = c(1952, 2007), 
 								 						format = "####", 
 								 						step = 5)
 								 ),
 		mainPanel(#"My cool graphs will go here",
-							h3(textOutput("output_country")),
+							h3(textOutput("output_countries_years")),
 							textOutput("info"),
-							plotOutput("ggplot_gdpPercap_vs_country"),
+							plotOutput("ggplot_gdpPercap_vs_two_countries"),
 							tableOutput("gapminder_table"))
 		)
 	))
